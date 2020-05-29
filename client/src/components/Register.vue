143695efc2dc44c8f30dd2e6f995ca70eff1d546
ameCont <americancont7@gmail.com>
2020-05-29 16:08:03 +0300

    v48:14

---
+++ client/src/components/Register.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>   
+    <h1>Register</h1>
+
+    <input
+    type="email"
+    name="email"
+    placeholder="email"/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { 
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+/* eslint-disable */
