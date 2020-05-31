622daa4800ecd402c41aa5c66b5a8ed7bb4c99b0
ameCont <americanCont7@gmail.com>
2020-05-31 18:45:08 -0400

    configs updated

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 //const config = require('../config/config')
-const { config, otherConfigs } = require =('../config/config')
+const { config, otherConfigs } = require('../config/config')
 const db = {}
 
 //console.log(config)
